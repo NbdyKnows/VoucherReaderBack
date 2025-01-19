357bd779b1b6a02eabeb639b5dad1c4382e52406
Claudio Jara <holasoyarturo123.com@gmail.com>
2025-01-18 21:45:18 -0500

    funca todo lo basico

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { SupabaseClient, createClient } from '@supabase/supabase-js';
+import { v4 as uuidv4 } from 'uuid';
+import { Express } from 'express';
 import Groq from 'groq-sdk';
 import * as fs from 'fs';
 import * as path from 'path';
@@ -33,46 +35,51 @@ export class AppService {
     const base64Image = this.encodeImage(filePath);
     console.log('Base64 image:', base64Image); // Verificar la imagen codificada
 
-    // Crear la solicitud a Groq
-    const result = await this.groqClient.chat.completions.create({
-      model: 'llama-3.2-90b-vision-preview',
-      messages: [
-        {
-          role: 'user',
-          content: [
-            { type: 'text', text: "Envia un JSON de los datos del voucher: name, operationNumber, amount(el número que va despues del S/), date" },
-            {
-              type: 'image_url',
-              image_url: {
-                url: `data:image/jpeg;base64,${base64Image}`,
+    try {
+      // Crear la solicitud a Groq
+      const result = await this.groqClient.chat.completions.create({
+        model: 'llama-3.2-90b-vision-preview',
+        messages: [
+          {
+            role: 'user',
+            content: [
+              { type: 'text', text: "Envia un JSON de los datos del voucher: name, operationNumber, amount(el número que va despues del S/), date" },
+              {
+                type: 'image_url',
+                image_url: {
+                  url: `data:image/jpeg;base64,${base64Image}`,
+                },
               },
-            },
-          ],
-        },
-      ],
-      temperature: 1,
-      max_completion_tokens: 1024,
-      top_p: 1,
-      stream: false,
-      response_format: { type: 'json_object' },
-      stop: null,
-    });
+            ],
+          },
+        ],
+        temperature: 1,
+        max_completion_tokens: 1024,
+        top_p: 1,
+        stream: false,
+        response_format: { type: 'json_object' },
+        stop: null,
+      });
 
-    const detections = result.choices[0]?.message?.content;
-    console.log('Detections:', detections); // Verificar las detecciones
+      const detections = result.choices[0]?.message?.content;
+      console.log('Detections:', detections); // Verificar las detecciones
 
-    if (!detections || detections.length === 0) {
-      throw new HttpException('No text detected', HttpStatus.BAD_REQUEST);
+      if (!detections || detections.length === 0) {
+        throw new HttpException('No text detected', HttpStatus.BAD_REQUEST);
+      }
+
+      const extractedData = this.extractImportantData(detections);
+      console.log('Extracted data:', extractedData); // Verificar los datos extraídos
+
+      if (!extractedData.amount || !extractedData.date || !extractedData.operationNumber || !extractedData.name) {
+        return { success: false, message: 'Información Soportada no Encontrada (nombre, monto, fecha y número de operación)', data: extractedData };
+      }
+
+      return { success: true, data: extractedData };
+    } catch (error) {
+      console.error('Error processing image:', error);
+      throw new HttpException('Error processing image', HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
-    const extractedData = this.extractImportantData(detections);
-    console.log('Extracted data:', extractedData); // Verificar los datos extraídos
-
-    if (!extractedData.amount || !extractedData.date || !extractedData.operationNumber || !extractedData.name) {
-      return { success: false, message: 'Información Soportada no Encontrada (nombre, monto, fecha y número de operación)', data: extractedData };
-    }
-
-    return { success: true, data: extractedData };
   }
 
   async saveVoucherData(extractedData: { name: string, amount: number, date: string, operationNumber: string }) {
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,14 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(bodyParser.json({ limit: '5mb' }));
   app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
+
+  // Habilitar CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', // Cambia esto al origen de tu frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
